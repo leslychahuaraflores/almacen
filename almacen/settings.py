d25d3c3c08bc4af847399c541f6a452bc8be6bbc
lesly <chahuarafloreslesly@gmail.com>
2017-11-15 01:31:38 -0500

    database

--- almacen/settings.py
+++ almacen/settings.py
@@ -77,10 +77,21 @@ WSGI_APPLICATION = 'almacen.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-DATABASES = {
+DATABASESx = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'mydatabase',
+        'USER': 'mydatabaseuser',
+        'PASSWORD': 'mypassword',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
